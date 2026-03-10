167a416465575e8cd48ab99a35e94bade919e2ac
RS <69071162+Arrus218@users.noreply.github.com>
2026-03-10 22:31:02 +0800

    Create player and staff classes

---
+++ src/main/java/seedu/address/model/person/Player.java
@@ -0,0 +1,11 @@
+package seedu.address.model.person;
+
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+public class Player extends Person {
+    public Player(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        super(name, phone, email, address, tags, Role.PLAYER);
+    }
+}
---
+++ src/main/java/seedu/address/model/person/Staff.java
@@ -0,0 +1,11 @@
+package seedu.address.model.person;
+
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+public class Staff extends Person {
+    public Staff(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        super(name, phone, email, address, tags, Role.STAFF);
+    }
+}
