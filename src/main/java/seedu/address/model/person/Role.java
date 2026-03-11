6dcebf185f220c5172f2c541a69e3e235b1744c9
Barnabas Sng <barnabassng10@gmail.com>
2026-03-11 23:25:42 +0800

    Add checking for valid role functionality in enum role

--- src/main/java/seedu/address/model/person/Role.java
+++ src/main/java/seedu/address/model/person/Role.java
@@ -4,5 +4,17 @@ package seedu.address.model.person;
  * Represents the role of a person in the address book.
  */
 public enum Role {
-    PLAYER, STAFF
+    PLAYER, STAFF;
+
+    /**
+     * Returns if a given string is a valid role.
+     */
+    public static boolean isValidRole(String test) {
+        for (Role role : Role.values()) {
+            if (role.name().equals(test)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
